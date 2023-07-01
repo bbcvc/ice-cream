f09a1d4f3146a9c3f75044f5bd6f832251b3e13a
chenglong <chenglong9604@outlook.com>
2023-07-01 17:47:38 +0800

    chore: update

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -9,10 +9,29 @@ const config = new Configuration({
 const openai = new OpenAIApi(config)
 
 export default async function handler(req: Request, res: Response) {
+
+  let { prompt: content } = await req.json();
+
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages: [{ role: 'user', content: 'What is love?' }]
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that continues existing text based on context from prior text. " +
+          "Give more weight/priority to the later characters than the beginning ones. " +
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences.",
+        // we're disabling markdown for now until we can figure out a way to stream markdown text with proper formatting: https://github.com/steven-tey/novel/discussions/7
+        // "Use Markdown formatting when appropriate.",
+      },
+      { role: 'user', content },
+    ],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    n: 1,
   })
   const stream = OpenAIStream(response)
   return new StreamingTextResponse(stream)
