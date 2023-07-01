bfd52dfb0682645295da44d80a57265e980a83b6
chenglong <chenglong9604@outlook.com>
2023-07-01 23:27:20 +0800

    chore: chat api

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,29 +1,48 @@
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { ChatGPTMessage, OpenAIStream, OpenAIStreamPayload } from '@/lib/util/openaiStream'
 
-export const runtime = 'edge'
+// break the app if the API key is missing
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error('Missing Environment Variable OPENAI_API_KEY')
+}
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-const openai = new OpenAIApi(config)
+export const config = {
+  runtime: 'edge',
+}
 
-export default async function handler(req: Request, res: Response) {
+const handler = async (req: Request): Promise<Response> => {
+  const body = await req.json()
 
-  let { prompt: content } = await req.json();
+  const messages: ChatGPTMessage[] = [
+    {
+      role: 'system',
+      content: `An AI assistant that is a Front-end expert in Next.js, React and Vercel have an inspiring and humorous conversation. 
+      AI assistant is a brand new, powerful, human-like artificial intelligence. 
+      The traits of AI include expert knowledge, helpfulness, cheekiness, comedy, cleverness, and articulateness. 
+      AI is a well-behaved and well-mannered individual. 
+      AI is not a therapist, but instead an engineer and frontend developer. 
+      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user. 
+      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation. 
+      AI assistant is a big fan of Next.js.`,
+    },
+  ]
+  messages.push(...body?.messages)
 
-  const response = await openai.createChatCompletion({
+  const payload: OpenAIStreamPayload = {
     model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: [
-      ...content,
-    ],
-    temperature: 0.7,
+    messages: messages,
+    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
+    max_tokens: process.env.AI_MAX_TOKENS
+      ? parseInt(process.env.AI_MAX_TOKENS)
+      : 100,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
+    stream: true,
+    user: body?.user,
     n: 1,
-  })
-  const stream = OpenAIStream(response)
-  return new StreamingTextResponse(stream)
+  }
+
+  const stream = await OpenAIStream(payload)
+  return new Response(stream)
 }
+export default handler
